d89a221277dae1b6637168089e69427b883483bf
farazkhanfk7 <farazkhan138@gmail.com>
2022-02-14 13:47:57 +0000

    prod change: event img link

--- core/settings.py
+++ core/settings.py
@@ -246,9 +246,6 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = '/home/cssamu/css-amu-server/assets'
 
-MEDIA_URL = 'images/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'images')
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
